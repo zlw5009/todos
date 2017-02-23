c777ea9a214d5d195750897bf1d36e154526ac8d
Zachary Williams <zlw5009@gmail.com>
2017-02-22 21:37:51 -0600

    Created todo project and added sessions to create a new todo list

---
+++ todo.rb
@@ -0,0 +1,30 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get "/" do
+  redirect "/lists"
+end
+
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+get "/lists/new" do
+  erb :new_list
+end
+
+post "/lists" do
+  session[:lists] << {name: params[:list_name], todos: []}
+  redirect "/lists"
+end
